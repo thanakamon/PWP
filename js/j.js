e97b317d95e340f250a62ee6fd8f2bd6cc078983
thanakamon <42264205+thanakamon@users.noreply.github.com>
2020-04-22 18:46:55 +0700

    p1

---
+++ js/j.js
@@ -0,0 +1,24 @@
+function playSlideShow(id) {
+    function show(n) {
+
+       var imgs = document.getElementById(id).getElementsByTagName(`img`);
+       var l = imgs.length;
+       
+       n = n == l ? 0 : n;
+   
+        for (var a = 0; a < l; a++) {
+            var img = imgs[a];
+            if (a == n) {
+                 img.className = `select`;
+            } else {
+                 img.className = ``;
+            }
+       }
+       return n;
+    }
+    var position = show(0);
+    window.setInterval(function() {
+        position = show(position + 1);
+    }, 2000);
+}
+playSlideShow(`ss_demo`);
